d660509d5f15ecb689c0cd425357bb0ef4b14d1d
AndrewWaddell <106304665+AndrewWaddell@users.noreply.github.com>
2022-12-23 15:03:26 +1100

    first comment
    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        // My first comment is to check that git will receive the changes.
     }
 }

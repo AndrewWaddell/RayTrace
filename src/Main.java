b106efd46e8b9c9811de61357951f96a5332e6ff
AndrewWaddell <106304665+AndrewWaddell@users.noreply.github.com>
2022-12-24 11:51:23 +1100

    My first class instantiation

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
 
         System.out.println("Hello world!");
         // My first comment is to check that git will receive the changes.
+
+        Shape myshape = new Shape();
+        myshape.printElsewhere();
     }
     public static void traceFrame(){
         // trace function but only one iteration
--- src/Shape.java
+++ src/Shape.java
@@ -1,5 +1,9 @@
 public class Shape {
     public static void changeOfBasis(){
         //can I talk to other classes?
+
+    }
+    public static void printElsewhere(){
+        System.out.println("success");
     }
 }

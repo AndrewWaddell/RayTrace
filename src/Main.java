0ed39369db9d62d57dba5b33d316e6a58c40e8f1
AndrewWaddell <106304665+AndrewWaddell@users.noreply.github.com>
2022-12-24 12:05:28 +1100

    First Matrix

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
         System.out.println("Hello world!");
         // My first comment is to check that git will receive the changes.
-
+        int size = 4;
         Shape myshape = new Shape();
-        myshape.printElsewhere();
+        myshape.matrixManipulation(size);
     }
     public static void traceFrame(){
         // trace function but only one iteration
--- src/Shape.java
+++ src/Shape.java
@@ -3,7 +3,14 @@ public class Shape {
         //can I talk to other classes?
 
     }
-    public static void printElsewhere(){
-        System.out.println("success");
+    public static void matrixManipulation(int s){
+        int[][] sqMatrix = new int[s][s];
+        System.out.println("My zeroed matrix:");
+        for (int i = 0; i < s; i++){
+            for (int j = 0; j<s;j++){
+                System.out.print(sqMatrix[i][j]+"\t");
+            }
+            System.out.println();
+        }
     }
 }

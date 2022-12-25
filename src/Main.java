6c08551fe1181eb78352d8a7ae9f6f26f8d58a5a
AndrewWaddell <106304665+AndrewWaddell@users.noreply.github.com>
2022-12-25 20:33:40 +1100

    Populate matrix
    Now we can add values to the matrix by rows and columns

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        // My first comment is to check that git will receive the changes.
-        int size = 2;
-        Shape myshape = new Shape();
-        myshape.matrixManipulation(size);
         Matrix2d x = new Matrix2d();
-        int i = 2;
+        int[] size = {2,2};
+        x.create(size);
+
+        int rowIndex = 1;
         float[] row = {1,2};
-        int[] size2 = {2,2};
-        x.populateRow(i,row);
-        x.create(size2);
-        x.populateRow(i,row);
+        float[] col = {3,9};
+        x.populateRow(rowIndex,row);
+        x.populateCol(rowIndex,col);
+        x.print();
     }
     public static void traceFrame(){
         // trace function but only one iteration
--- src/Matrix2d.java
+++ src/Matrix2d.java
@@ -4,22 +4,50 @@ public class Matrix2d {
     boolean created = false;
     int numRows;
     int numCols;
-    float vals;
+    float[][] vals;
     public void create(int[] size){
         numRows = size[0];
         numCols = size[1];
-        float[][] vals = new float[numRows][numCols];
+        vals = new float[numRows][numCols];
         created = true;
     }
     public void populateRow(int i, float[] row){
         // i is the index of the row that we want to insert into the matrix
         // row contains all the values in the row
         if (!created){
-            System.out.println("0");
+            System.out.println("Matrix doesn't exist");
+            return;
+        }
+        if (row.length != numCols){
+            System.out.println("Row is incorrect length");
             return;
         }
         for(int j=0;j<numCols;j++){
-            System.out.println("1");
+            vals[i][j] = row[j];
+        }
+    }
+    public void populateCol(int j, float[] col){
+        // j is the index of the column that we want to insert into the matrix
+        // col contains all the values in the column
+        if (!created){
+            System.out.println("Matrix doesn't exist");
+            return;
+        }
+        if (col.length != numRows){
+            System.out.println("Column is incorrect length");
+            return;
+        }
+        for(int i=0;i<numRows;i++){
+            vals[i][j] = col[i];
+        }
+    }
+    public void print(){
+        for (int i=0;i<numRows;i++){
+            for(int j=0;j<numCols;j++){
+                String val = String.valueOf(vals[i][j]);
+                System.out.print(val+'\t');
+            }
+            System.out.println();
         }
     }
     public void multiply(){

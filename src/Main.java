b1c394cc2c68e946400c34b30dafdb1b570ca5f8
AndrewWaddell <106304665+AndrewWaddell@users.noreply.github.com>
2022-12-27 17:31:44 +1100

    Created
    Two ways to create a matrix. Check if matrix is created within each of its methods. Return a strupid value if method requires a return value. There is a bug in the raise to the power of method identified with a print line.

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,17 @@ public class Main {
         Matrix2d mat3 = new Matrix2d();
 
         int[] size = {2,2};
-        mat1.create(size);
-        mat2.create(size);
-        mat3.create(size);
+        float[] row = {3,3};
+        int[] num = {0,1,2};
 
-        float[] row1 = {1,2};
-        float[] row2 = {3,4};
-        float[] col1 = {5,6};
-        float[] col2 = {7,8};
-
-        int row1i = 0;
-        int row2i = 1;
-        int col1i = 0;
-        int col2i = 1;
-
-        mat1.populateRow(row1i,row1);
-        mat1.populateRow(row2i,row2);
-        mat2.populateCol(col1i,col1);
-        mat2.populateCol(col2i,col2);
-
-        System.out.println("Mat1");
+        mat1.createEmpty(size);
+        mat1.populateRow(num[0],row);
+        mat1.populateRow(num[1],row);
+        System.out.println("three squared is not 81");
+        mat1.raisePower(num[2]);
         mat1.print();
-        System.out.println("Mat2");
-        mat2.print();
 
 
-        mat3.inherit(mat1.add(mat2));
-        System.out.println("Addition");
-        mat3.print();
-
-        System.out.println("dot multiply");
-        mat3.inherit(mat1.multiplyPiecewise(mat1));
-        mat3.print();
-
-        System.out.println("divide");
-        int power = -2;
-        mat3.inherit(mat1.raisePower(power));
-        mat3.print();
     }
     public static void traceFrame(){
         // trace function but only one iteration
--- src/Matrix2d.java
+++ src/Matrix2d.java
@@ -6,36 +6,34 @@ public class Matrix2d {
     int numRows;
     int numCols;
     float[][] vals;
-    public void create(int[] sizeInput){
+    public void createEmpty(int[] sizeInput){
         size = sizeInput;
         numRows = size[0];
         numCols = size[1];
         vals = new float[numRows][numCols];
         created = true;
     }
-    public void inherit(float[][] inMat){
+    public void create(float[][] inMat){
         vals = inMat;
+        numRows = inMat.length;
+        numCols = inMat[0].length;
+        size = new int[]{numRows,numCols};
+        created = true;
     }
     public void populateRow(int i, float[] row){
         // i is the index of the row that we want to insert into the matrix
         // row contains all the values in the row
         if (!created){
-            System.out.println("Matrix doesn't exist");
+            System.out.println("Matrix not created");
             return;
         }
-        if (row.length != numCols){
-            System.out.println("Row is incorrect length");
-            return;
-        }
-        for(int j=0;j<numCols;j++){
-            vals[i][j] = row[j];
-        }
+        vals[i] = row;
     }
     public void populateCol(int j, float[] col){
         // j is the index of the column that we want to insert into the matrix
         // col contains all the values in the column
         if (!created){
-            System.out.println("Matrix doesn't exist");
+            System.out.println("Matrix not created");
             return;
         }
         if (col.length != numRows){
@@ -47,6 +45,10 @@ public class Matrix2d {
         }
     }
     public void fillWithItem(float item) {
+        if (!created){
+            System.out.println("Matrix not created");
+            return;
+        }
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
                 vals[i][j] = item;
@@ -54,6 +56,10 @@ public class Matrix2d {
         }
     }
     public void print(){
+        if (!created){
+            System.out.println("Matrix not created");
+            return;
+        }
         for (int i=0;i<numRows;i++){
             for(int j=0;j<numCols;j++){
                 String val = String.valueOf(vals[i][j]);
@@ -63,6 +69,11 @@ public class Matrix2d {
         }
     }
     public float[][] add(Matrix2d mat2){
+        if (!created){
+            System.out.println("Matrix not created");
+            float[][] zero = new float[1][1];
+            return zero;
+        }
         float[][] result = new float[numRows][numCols];
         for (int i=0;i<numRows;i++){
             for (int j=0;j<numCols;j++){
@@ -71,7 +82,48 @@ public class Matrix2d {
         }
         return result;
     }
+    public float[][] subtract(Matrix2d mat2){
+        if (!created){
+            System.out.println("Matrix not created");
+            float[][] zero = new float[1][1];
+            return zero;
+        }
+        float[][] result = new float[numRows][numCols];
+        for (int i=0;i<numRows;i++){
+            for (int j=0;j<numCols;j++){
+                result[i][j] = vals[i][j] - mat2.vals[i][j];
+            }
+        }
+        return result;
+    }
+    public void addValue(float value){
+        if (!created){
+            System.out.println("Matrix not created");
+            return;
+        }
+        for (int i=0;i<numRows;i++){
+            for (int j=0;j<numCols;j++){
+                vals[i][j] = value;
+            }
+        }
+    }
+    public void multiplyBy(float value){
+        if (!created){
+            System.out.println("Matrix not created");
+            return;
+        }
+        for (int i=0;i<numRows;i++){
+            for (int j=0;j<numCols;j++){
+                vals[i][j] = vals[i][j]*value;
+            }
+        }
+    }
     public float[][] multiplyPiecewise(Matrix2d mat2){
+        if (!created){
+            System.out.println("Matrix not created");
+            float[][] zero = new float[1][1];
+            return zero;
+        }
         float[][] result = new float[numRows][numCols];
         for (int i=0;i<numRows;i++){
             for (int j=0;j<numCols;j++){
@@ -81,8 +133,13 @@ public class Matrix2d {
         return result;
     }
     public float[][] raisePower(int POWER){
+        if (!created){
+            System.out.println("Matrix not created");
+            float[][] zero = new float[1][1];
+            return zero;
+        }
         Matrix2d ones = new Matrix2d();
-        ones.create(size);
+        ones.createEmpty(size);
         float one = 1;
         ones.fillWithItem(one);
         float[][] result = ones.vals;

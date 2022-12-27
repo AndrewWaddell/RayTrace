3ccabdfd9ac873e095e4b74bcafc4a74c39b5e28
AndrewWaddell <106304665+AndrewWaddell@users.noreply.github.com>
2022-12-27 16:50:44 +1100

    Piecewise matrix
    gave matrix functions that allow matrix manipulation piece-wise

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        Matrix2d x = new Matrix2d();
-        int[] size = {2,2};
-        x.create(size);
+        Matrix2d mat1 = new Matrix2d();
+        Matrix2d mat2 = new Matrix2d();
+        Matrix2d mat3 = new Matrix2d();
 
-        int rowIndex = 1;
-        float[] row = {1,2};
-        float[] col = {3,9};
-        x.populateRow(rowIndex,row);
-        x.populateCol(rowIndex,col);
-        x.print();
+        int[] size = {2,2};
+        mat1.create(size);
+        mat2.create(size);
+        mat3.create(size);
+
+        float[] row1 = {1,2};
+        float[] row2 = {3,4};
+        float[] col1 = {5,6};
+        float[] col2 = {7,8};
+
+        int row1i = 0;
+        int row2i = 1;
+        int col1i = 0;
+        int col2i = 1;
+
+        mat1.populateRow(row1i,row1);
+        mat1.populateRow(row2i,row2);
+        mat2.populateCol(col1i,col1);
+        mat2.populateCol(col2i,col2);
+
+        System.out.println("Mat1");
+        mat1.print();
+        System.out.println("Mat2");
+        mat2.print();
+
+
+        mat3.inherit(mat1.add(mat2));
+        System.out.println("Addition");
+        mat3.print();
+
+        System.out.println("dot multiply");
+        mat3.inherit(mat1.multiplyPiecewise(mat1));
+        mat3.print();
+
+        System.out.println("divide");
+        int power = -2;
+        mat3.inherit(mat1.raisePower(power));
+        mat3.print();
     }
     public static void traceFrame(){
         // trace function but only one iteration
--- src/Matrix2d.java
+++ src/Matrix2d.java
@@ -2,14 +2,19 @@ import javax.sound.sampled.EnumControl;
 
 public class Matrix2d {
     boolean created = false;
+    int[] size;
     int numRows;
     int numCols;
     float[][] vals;
-    public void create(int[] size){
+    public void create(int[] sizeInput){
+        size = sizeInput;
         numRows = size[0];
         numCols = size[1];
         vals = new float[numRows][numCols];
         created = true;
+    }
+    public void inherit(float[][] inMat){
+        vals = inMat;
     }
     public void populateRow(int i, float[] row){
         // i is the index of the row that we want to insert into the matrix
@@ -41,6 +46,13 @@ public class Matrix2d {
             vals[i][j] = col[i];
         }
     }
+    public void fillWithItem(float item) {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                vals[i][j] = item;
+            }
+        }
+    }
     public void print(){
         for (int i=0;i<numRows;i++){
             for(int j=0;j<numCols;j++){
@@ -50,8 +62,49 @@ public class Matrix2d {
             System.out.println();
         }
     }
-    public void multiply(){
-
+    public float[][] add(Matrix2d mat2){
+        float[][] result = new float[numRows][numCols];
+        for (int i=0;i<numRows;i++){
+            for (int j=0;j<numCols;j++){
+                result[i][j] = vals[i][j] + mat2.vals[i][j];
+            }
+        }
+        return result;
+    }
+    public float[][] multiplyPiecewise(Matrix2d mat2){
+        float[][] result = new float[numRows][numCols];
+        for (int i=0;i<numRows;i++){
+            for (int j=0;j<numCols;j++){
+                result[i][j] = vals[i][j] * mat2.vals[i][j];
+            }
+        }
+        return result;
+    }
+    public float[][] raisePower(int POWER){
+        Matrix2d ones = new Matrix2d();
+        ones.create(size);
+        float one = 1;
+        ones.fillWithItem(one);
+        float[][] result = ones.vals;
+        if (POWER<0){
+            for (int power=0;power>POWER;power--) {
+                for (int i = 0; i < numRows; i++) {
+                    for (int j = 0; j < numCols; j++) {
+                        result[i][j] = result[i][j] / vals[i][j];
+                    }
+                }
+            }
+        } else if (POWER>0) {
+            result = vals;
+            for (int power=1;power<POWER;power++) {
+                for (int i = 0; i < numRows; i++) {
+                    for (int j = 0; j < numCols; j++) {
+                        result[i][j] = result[i][j] * vals[i][j];
+                    }
+                }
+            }
+        }
+        return result;
     }
 
 }

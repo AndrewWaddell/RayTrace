9944d7b5f46b28f767a205031a8611bea51a393e
AndrewWaddell <106304665+AndrewWaddell@users.noreply.github.com>
2023-01-13 18:18:06 +1100

    unit not normalised
    will need to find out why the unit vectors aren't normalised. Also find a better way of plotting the list arrays. Would be nice to be able to actually plot them in 3d...

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("ray points");
         for (int i=0;i<opticalBench.rays.pointsAcc.size();i++){
             System.out.println(i);
-            opticalBench.rays.pointsAcc.get(i).print();
+            opticalBench.rays.unitAcc.get(i).print();
         }
         opticalBench.plot();
 
